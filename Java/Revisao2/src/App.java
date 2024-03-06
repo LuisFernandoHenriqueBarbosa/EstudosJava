b522981ac3e8b65abe0fed3407e51dcf1771b8d5
Aluno <aluno@fatecriopreto.edu.br.local>
2024-03-06 12:17:17 -0300

    updt1217

--- Java/Revisao2/src/App.java
+++ Java/Revisao2/src/App.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        String nome="", sexo="", curso="" , periodo="";
-        int ra,idade;
+        String nome = "", sexo = "", periodo = "", nomeVelha="", cursoNova="";
+        int tempIdade=0, tempIdade2=0, curso = 0, ra, idade, contInfo = 0, contAgro = 0, contDs = 0, contSexo = 0;
         Scanner teclado = new Scanner(System.in);
-        
-        do{
+
+        do {
             System.out.println("Digite seu Ra: ");
             ra = teclado.nextInt();
             System.out.println("Digite seu nome completo: ");
@@ -15,11 +15,27 @@ public class App {
             idade = teclado.nextInt();
             System.out.println("Informe seu sexo: M(masculino) ou F(Feminino)");
             sexo = teclado.next();
-            System.out.println("Informe o curso: I(Informática para negócios) A(Agronegócio) D(Analise e Desenvolvimento de Sistemas)");
-            curso = teclado.next();
+            System.out.println(
+                    "Informe o curso: 1(Informática para negócios) 2(Agronegócio) 3(Analise e Desenvolvimento de Sistemas)");
+            curso = teclado.nextInt();
             System.out.println("Informe o seu período: M(Manhã), T(Tarde) ou N(noturno)");
             periodo = teclado.next();
-        }while(ra != 0 && ra > 0);
-        
+            if (curso == 1) {
+                contInfo++;
+            } else if (curso == 2) {
+                contAgro++;
+            } else if (curso == 3) {
+                contDs++;
+            } else {
+                System.out.println("Curso não identificado");
+            }
+
+            if (sexo == "F") {
+                contSexo++;
+            }
+
+            
+        } while (ra != 0 && ra > 0);
+
     }
 }
