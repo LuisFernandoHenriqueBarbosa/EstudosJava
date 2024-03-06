79ba78d8d4a8d51ab0fe71e399f1edf88abec377
Aluno <aluno@fatecriopreto.edu.br.local>
2024-03-06 11:59:59 -0300

    updt1159

--- Java/Revisao2/src/App.java
+++ Java/Revisao2/src/App.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, MotherFuckers!");
+        String nome="", sexo="", curso="" , periodo="";
+        int ra,idade;
+        Scanner teclado = new Scanner(System.in);
+        
+        do{
+            System.out.println("Digite seu Ra: ");
+            ra = teclado.nextInt();
+            System.out.println("Digite seu nome completo: ");
+            nome = teclado.next();
+            System.out.println("Informe a sua idade");
+            idade = teclado.nextInt();
+            System.out.println("Informe seu sexo: M(masculino) ou F(Feminino)");
+            sexo = teclado.next();
+            System.out.println("Informe o curso: I(Informática para negócios) A(Agronegócio) D(Analise e Desenvolvimento de Sistemas)");
+            curso = teclado.next();
+            System.out.println("Informe o seu período: M(Manhã), T(Tarde) ou N(noturno)");
+            periodo = teclado.next();
+        }while(ra != 0 && ra > 0);
+        
     }
 }
