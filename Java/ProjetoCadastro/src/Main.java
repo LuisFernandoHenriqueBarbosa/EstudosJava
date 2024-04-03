026cc61b03c565bd1253b6729c7e5da517c18cf3
Luis Fernando <luisfernandohenriquebarbosa18@hotmail.com>
2024-04-03 10:33:45 -0300

    udpt1033

--- Java/ProjetoCadastro/src/Main.java
+++ Java/ProjetoCadastro/src/Main.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
 
-public class App {
+public class Main {
     public static void main(String[] args) throws Exception {
         Scanner ler = new Scanner(System.in);
         Cliente objCliente = new Cliente();
         
         System.out.println("Digite o seu nome: ");
         objCliente.nome = ler.next();
+        
 
         System.out.println("Digite o seu CPF: ");
         objCliente.cpf = ler.next();
@@ -28,5 +29,19 @@ public class App {
 
         ler.close();
     }
+    public static boolean ValidaCPF (String cpf) {
+        if(cpf. length() != 14){
+            return false;
+        }else{
+            return true;
+        }
+
+        if(cpf.charAt(3) != '.' && cpf.charAt(7) != '.' && cpf.charAt(11) != '-'){
+            return false;
+        }else{
+            return true;
+        }
+
+        if(cep)
     
 }
